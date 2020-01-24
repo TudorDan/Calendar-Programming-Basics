d72d56accd92d7b7b4abe02db975dd54936c2e94
TudorDan <tudor.ist@gmail.com>
2020-01-24 20:43:37 +0200

    Add saving files functionality

--- storage.py
+++ storage.py
@@ -16,3 +16,20 @@ def get_list_from(file_name):
         lines = file.readlines()
     current_list = [element.replace("\n", "").split(";") for element in lines]
     return current_list
+
+
+def write_list_to(file_name, new_list):
+    """
+    Writes list of lists into a txt file.
+
+    Args:
+        file_name (str): name of file to write to
+        new_list(list): list of lists to write to a file
+
+    Returns:
+         None
+    """
+    with open(file_name, "w") as file:
+        for record in new_list:
+            row = ';'.join(record)
+            file.write(row + "\n")
